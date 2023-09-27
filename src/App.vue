02a05a25366d4f662dfb544a5e5c172932fb5cc8
Uverit <web.newhorizon@gmail.com>
2023-09-27 23:36:57 +0200

    v0.0.1 - Project Setup & Cleanup
    Created the Portfolio project.
    
    Removed unnecessary code and files.
    
    Added the version from "package.json".

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+
+
+
+<template>
+
+    Portfolio
+    <p>v{{ appVersion }}</p>
+
+
+</template>
+
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { version } from '../package.json'
+
+export default defineComponent({
+    name: "App",
+
+    data(){
+        return{
+            appVersion: version
+        }
+    }
+
+});
+</script>
+
+
+
+<style lang="scss">
+
+
+
+</style>
